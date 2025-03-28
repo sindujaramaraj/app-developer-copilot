872c2dbcdda64016d88ff9994c65d46d1649d68d
Sinduja Ramaraj <sindujaramaraj@sindujas-mbp.lan>
2025-03-27 22:01:22 -0700

    update constants

--- src/builder/constants.ts
+++ src/builder/constants.ts
@@ -6,12 +6,12 @@ export const APP_CONVERSATION_FILE = 'conversation.json';
 export const APP_ARCHITECTURE_DIAGRAM_FILE = 'architecture.md';
 export const APP_STACK_DIAGRAM_FILE = 'stack.md';
 export const SUPA_SQL_FILE = 'supabase/migrations/init.sql';
-export const SUPA_TYPES_FILE = 'supabase/database.types.ts';
+export const SUPA_TYPES_FILE = 'src/supabase/database.types.ts';
 export const CLASS_DIAGRAM_FILE = 'classDiagram.md';
 export const PROJECT_TREE_DIAGRAM_FILE = 'projectTreeDiagram.md';
 export const MAX_RETRY_COUNT = 1;
 
-export const ENABLE_TELEMETRY = false; // Config to enable or disable telemetry collection
+export const ENABLE_TELEMETRY = true; // Config to enable or disable telemetry collection
 export const ENABLE_AUTHENTICATION = false; // Config to enable or disable authentication
 export const ENABLE_WEB_APP = true; // Config to enable or disable web app creation
 export const ENABLE_WEB_STACK_CONFIG = true; // Config to enable or disable web stack configuration
